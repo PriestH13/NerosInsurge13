ab66efb69f719d29728a307a1a7c3abe9a0f5f77
PriestH13 <mateo.prifti13@gmail.com>
2025-05-29 23:56:45 +0200

    *Implement petitions app basic CRUD with views, urls,forms, and templates *Create partial for footer and navbar

--- petitions/urls.py
+++ petitions/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
-from . import views
+from .views import (
+    HomeView,
+    PetitionListView,
+    PetitionDetailView,
+    PetitionCreateView,
+    PetitionUpdateView,
+    PetitionDeleteView
+)
 
+app_name = 'petitions'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
+
+    
+    path('list/', PetitionListView.as_view(), name='petition_list'),
+    path('create/', PetitionCreateView.as_view(), name='petition_create'),
+    path('<int:pk>/', PetitionDetailView.as_view(), name='petition_detail'),
+    path('<int:pk>/edit/', PetitionUpdateView.as_view(), name='petition_edit'),
+    path('<int:pk>/delete/', PetitionDeleteView.as_view(), name='petition_delete'),
 ]
--- petitions/views.py
+++ petitions/views.py
@@ -1,5 +1,100 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Count, Q
+from .models import Petition, PetitionStatus, PetitionCategory
+from .forms import PetitionForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class HomeView(ListView):
+    model = Petition
+    template_name = 'base.html'  # O cambia col tuo template corretto
+    context_object_name = 'petitions'
+    paginate_by = 6
+
+    def get_queryset(self):
+        return Petition.objects.filter(status=PetitionStatus.PUBLISHED, is_active=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = PetitionCategory.objects.all()
+        return context
 
 
-def home(request):
-    return render(request, "petitions/home.html")
+class PetitionListView(ListView):
+    model = Petition
+    template_name = 'petitions/petition_list.html'
+    context_object_name = 'petitions'
+    paginate_by = 6
+
+    def get_queryset(self):
+        qs = Petition.objects.filter(status=PetitionStatus.PUBLISHED, is_active=True)
+
+        # Filtro ricerca
+        q = self.request.GET.get('q')
+        if q:
+            qs = qs.filter(Q(title__icontains=q) | Q(description__icontains=q))
+
+        # Filtro categoria
+        category = self.request.GET.get('category')
+        if category:
+            qs = qs.filter(category__name__iexact=category)
+
+        # Ordinamento
+        order = self.request.GET.get('order')
+        if order == 'recenti':
+            qs = qs.order_by('-created_at')
+        elif order == 'popolari':
+            qs = qs.annotate(num_signatures=Count('signatures')).order_by('-num_signatures')
+        else:
+            qs = qs.order_by('-created_at')
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = PetitionCategory.objects.all()
+        return context
+
+
+class PetitionDetailView(DetailView):
+    model = Petition
+    template_name = 'petitions/petition_detail.html'
+    context_object_name = 'petition'
+
+    def get_queryset(self):
+        return Petition.objects.all()
+
+
+class PetitionCreateView(LoginRequiredMixin, CreateView):
+    model = Petition
+    fields = ['title', 'description', 'category']
+    template_name = 'petitions/petition_form.html'
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user  # ✅ aggiungi questa riga
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('petitions:petition_detail', kwargs={'pk': self.object.pk})
+
+
+
+class PetitionUpdateView(UpdateView):
+    model = Petition
+    form_class = PetitionForm
+    template_name = 'petitions/petition_form.html'
+
+    def get_queryset(self):
+        return Petition.objects.all()
+
+    def get_success_url(self):
+        return reverse_lazy('petitions:petition_detail', kwargs={'pk': self.object.pk})
+
+
+class PetitionDeleteView(DeleteView):
+    model = Petition
+    template_name = 'petitions/petition_confirm_delete.html'
+    success_url = reverse_lazy('petitions:petition_list')
+
+    def get_queryset(self):
+        return Petition.objects.all()
