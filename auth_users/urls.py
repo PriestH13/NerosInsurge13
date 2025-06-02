69d28f0607a41610d61d96d46228dd837d62055e
PriestH13 <mateo.prifti13@gmail.com>
2025-06-03 01:12:18 +0200

    add password change

--- auth_users/urls.py
+++ auth_users/urls.py
@@ -18,4 +18,8 @@ urlpatterns = [
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'),name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'),name='password_reset_confirm'),
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'),name='password_reset_complete'),
+    
+    # Password change
+    path('password_change/',auth_views.PasswordChangeView.as_view(template_name='auth/password_change_form.html',success_url=reverse_lazy('auth:password_change_done')),name='password_change'),
+    path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(template_name='auth/password_change_done.html'),name='password_change_done'),
 ]
