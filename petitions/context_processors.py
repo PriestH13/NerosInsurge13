2ae1699ea776665b8a6ddc88abc3a40984529291
PriestH13 <mateo.prifti13@gmail.com>
2025-06-29 00:10:32 +0200

    *Added context processor for featured_petitions and fixed carousel display on all pages

--- petitions/context_processors.py
+++ petitions/context_processors.py
@@ -1,6 +1,18 @@
+from .models import Petition, PetitionStatus
+from django.db.models import Count
+
 def unread_notifications_count(request):
     if request.user.is_authenticated:
         return {
             'unread_notifications_count': request.user.notifications.filter(is_read=False).count()
         }
     return {}
+
+
+def featured_petitions_context(request):
+    return {
+        'featured_petitions': Petition.objects.filter(
+            status=PetitionStatus.PUBLISHED,
+            is_active=True
+        ).annotate(num_signatures=Count('signatures')).order_by('-num_signatures')[:5]
+    }
